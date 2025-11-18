781eb6fddbe80ec8dce4f3c9fb9df162ec5c45d4
Spascheun <236190940+Spascheun@users.noreply.github.com>
2025-11-18 01:09:25 +0100

    using signal instead of except to handle close requests

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import API
+import sys
+import signal
 
+def signal_handler(sig, frame):
+    print("Signal received, closing program...")
+    api.close()
+    sys.exit(0)
 
 if __name__ == "__main__":
     api = API.webAPI()
-    try :
-        api.start()
-        while True:
-            pass
-    except KeyboardInterrupt:
-        print("KeyboardInterrupt received")
-        api.close()
+    signal.signal(signal.SIGINT, signal_handler) 
+    signal.signal(signal.SIGTERM, signal_handler)
+    api.start()
+    while True:
+        pass
